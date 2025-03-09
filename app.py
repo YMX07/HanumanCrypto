0a5fffcd7f39eb0ff0a55806bc0ffa0adb0212b2
YMX07 <82724028+YMX07@users.noreply.github.com>
2025-03-09 14:36:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ app.config.update(
 )
 
 # Initialize Celery
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'], backend=app.config['CELERY_RESULT_BACKEND'])
+celery = Celery(app.name, broker=redis_url, backend=redis_url)
 celery.conf.update(app.config)
 
 # Binance API client - Use environment variables for credentials
